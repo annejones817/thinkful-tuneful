45fbb2dbe20ee6422c45161ad9465c100dc56ab7
Anne Jones <annejones817@gmail.com>
2016-12-12 17:12:34 +0000

    Updated API post functionality and testing

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -37,4 +37,65 @@ class TestAPI(unittest.TestCase):
         # Delete test upload folder
         shutil.rmtree(upload_path())
 
-
+    def test_get_songs(self):
+        stronger = models.File(name="stronger.mp3")
+        song = models.Song()
+        stronger.song = song
+        
+        bornThisWay = models.File(name="bornThisWay.mp3")
+        song2 = models.Song()
+        bornThisWay.song = song2
+        
+        session.add_all([stronger, song, bornThisWay, song2])
+        session.commit()
+        
+        response = self.client.get("/api/songs", headers=[("Accept", "application/json")])
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+        
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(len(data), 2)
+        
+        stronger = data[0]
+        self.assertEqual(stronger["file"]["name"], "stronger.mp3")
+        
+        bornThisWay = data[1]
+        self.assertEqual(bornThisWay["file"]["name"], "bornThisWay.mp3")
+        
+    def test_post_song(self): 
+        """Adding a new song"""
+        
+        #Create file
+        stronger = models.File(name="stronger.mp3")
+        session.add(stronger)
+        session.commit()
+        
+        #Create data for post
+        data = {
+            "file": {
+                "id": 1
+            }
+        }
+        
+        response = self.client.post("/api/songs", 
+            data=json.dumps(data), 
+            content_type = "application/json", 
+            headers = [("Accept", "application/json")]
+        )
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(urlparse(response.headers.get("Location")).path, "/api/songs/1" )
+        
+        data = json.loads(response.data.decode("ascii"))
+        self.assertEqual(data["file"]["id"], 1)
+        self.assertEqual(data["file"]["name"], "stronger.mp3")
+        self.assertEqual(data["id"], 1)
+        
+        songs = session.query(models.Song).all()
+        self.assertEqual(len(songs), 1)
+        
+        song = songs[0]
+        self.assertEqual(song.file.name, "stronger.mp3")
+        self.assertEqual(song.id, 1)
--- tuneful/api.py
+++ tuneful/api.py
@@ -11,4 +11,74 @@ from tuneful import app
 from .database import session
 from .utils import upload_path
 
+#JSON Schema describing structure of post
+song_schema = {
+    "properties" : {
+        "file": {"type" : "object"},
+        "id" : {"type" : "integer"}
+    }, 
+    "required": ["file", "id"]
+}
 
+@app.route("/api/songs", methods=["GET"])
+@decorators.accept("application/json")
+def songs_get(): 
+    """Get a list of songs"""
+    songs = session.query(models.Song).order_by(models.Song.id)
+    
+    #convert songs to JSON and return response
+    data = json.dumps([song.as_dictionary() for song in songs])
+    print(data)
+    return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/songs/<int:id>", methods=["GET"])    
+@decorators.accept("application/json")
+def songs_get_by_id(id):
+    """Single song endpoint"""
+    #Get the song from the database
+    
+    song=session.query(models.Song).get(id)
+    
+    #Check whether song exists, if not return a 404 with a helpful message
+    
+    if not song: 
+        message="Could not find song with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+        
+    data = json.dumps(song.as_dictionary())
+    return Response(data, 200, mimetype="application/json")    
+    
+@app.route("/api/songs", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def songs_post(): 
+    """Add a new song"""
+    data = request.json
+    
+    """
+    #Check for valid JSON, if not return 422 Unprocessable
+    try: 
+        validate(data, song_schema)
+    except ValidationError as error: 
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+        """
+    
+    #Check that the id exists
+    if not session.query(models.File).filter(models.File.id == data["file"]["id"]).first():
+        message = "Could not find a song file with id {}".format(data["file"]["id"])
+        data = {"message": message}
+    
+    #Add the song 
+    song = models.Song()
+    file = session.query(models.File).filter(models.File.id == data["file"]["id"]).first()
+    file.song = song
+    song.file_id = file.id
+    session.add(song)
+    session.commit()
+    
+    #Return 201 Created
+    data = json.dumps(song.as_dictionary())
+    headers = {"Location": url_for("songs_get_by_id", id=song.id)}
+    return Response(data, 201, headers=headers, mimetype="application/json")
